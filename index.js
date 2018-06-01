d7c3a5b7f3bd71993adec95ea4d2db2aa3009591
Riaz Laskar <riazcool77@gmail.com>
2018-06-01 20:00:02 +0530

    path fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TailwindConverter = require('./../build/TailwindConverter');
+const TailwindConverter = require('./build/TailwindConverter');
 const converter = new TailwindConverter();
 module.exports = function(bsmarkup){
     return converter.setContent(bsmarkup).convert().get()
